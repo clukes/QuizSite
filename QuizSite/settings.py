d1bcdb39ddee441821ced0eae286cafe3c79bd36
Conner Lukes <clukes@me.com>
2022-02-21 18:01:55 +0000

    Update settings.py

--- QuizSite/settings.py
+++ QuizSite/settings.py
@@ -15,16 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a681$014uzjg3d2p%w9!+^*^!3yah^pwpfq7&bvrcy$*k*+c=e'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
